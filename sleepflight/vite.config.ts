c91f875f62531eecfcf3679cdb5e2df21da5131e
FutureThingsLab <futurethingslab0314@gmail.com>
2026-01-04 14:00:32 +0800

    update debug sleepflight

--- sleepflight/vite.config.ts
+++ sleepflight/vite.config.ts
@@ -9,6 +9,13 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+          input: path.resolve(__dirname, 'index.html'),
+        },
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
